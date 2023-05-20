13c3a2ab09eec3ce0588bde54762ef1b8d811e19
GEUNTAE SEO <74644453+gtseo0606@users.noreply.github.com>
2023-05-20 21:04:38 +0900

    Create 중복 제거하기.sql

---
+++ SQL/Programmers_SQL/sum_max_min/중복 제거하기.sql
@@ -0,0 +1,12 @@
+중복 제거하기
+동물 보호소에 들어온 동물의 이름은 몇 개인지 조회하는 SQL 문을 작성해주세요. 이때 이름이 NULL인 경우는 집계하지 않으며 중복되는 이름은 하나로 칩니다.
+
+-- MYSQL
+SELECT COUNT(NAME)
+FROM (SELECT DISTINCT NAME 
+        FROM ANIMAL_INS) AS A;
+
+-- ORACLE
+SELECT COUNT(NAME)
+FROM (SELECT DISTINCT NAME 
+        FROM ANIMAL_INS);
