6e48761059fb19c68bb32ddce42c02b6f51f9a50
GEUNTAE SEO <74644453+gtseo0606@users.noreply.github.com>
2023-06-16 00:08:15 +0900

    Create 루시와 엘라 찾기.sql

---
+++ SQL/Programmers_SQL/STRING, DATE/루시와 엘라 찾기.sql
@@ -0,0 +1,10 @@
+
+루시와 엘라 찾기
+
+동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 SQL 문을 작성해주세요.
+
+-- MYSQL, ORACLE
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID ASC;
