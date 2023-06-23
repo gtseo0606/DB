9448b7de0ea1ffe8a83a2c2cc4b9e8042cbd7afc
GEUNTAE SEO <74644453+gtseo0606@users.noreply.github.com>
2023-06-23 20:37:02 +0900

    Create 과일로 만든 아이스크림 고르기.sql

---
+++ SQL/Programmers_SQL/SELECT/과일로 만든 아이스크림 고르기.sql
@@ -0,0 +1,13 @@
+과일로 만든 아이스크림 고르기
+상반기 아이스크림 총주문량이 3,000보다 높으면서 아이스크림의 주 성분이 과일인 아이스크림의 맛을 총주문량이 큰 순서대로 조회하는 SQL 문을 작성해주세요.
+
+
+
+-- MYSQL. ORACLE
+SELECT A.FLAVOR
+FROM FIRST_HALF A
+JOIN ICECREAM_INFO B
+ON A.FLAVOR = B.FLAVOR
+WHERE A.TOTAL_ORDER > 3000
+AND B.INGREDIENT_TYPE = 'fruit_based'
+ORDER BY A.TOTAL_ORDER DESC;
