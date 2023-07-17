fac344ce10dfe3e30cd45b820c51df2dc292093e
GEUNTAE SEO <74644453+gtseo0606@users.noreply.github.com>
2023-07-17 21:24:32 +0900

    Create Weather Observation Station 18.sql

---
+++ SQL/HackerRank/Weather Observation Station 18.sql
@@ -0,0 +1,32 @@
+Weather Observation Station 18
+
+-- MYSQL
+WITH POINTS as (
+    SELECT
+        MIN(LAT_N) AS a,
+        MIN(LONG_W) AS b,
+        MAX(LAT_N) AS c,
+        MAX(LONG_W) AS d
+    FROM STATION
+)
+SELECT ROUND(ABS((a-c)+(b-d)),4)
+FROM POINTS
+
+
+-- MS SQL SERVER
+SELECT CAST(ABS((max(LAT_N) - min(LAT_N)) + (max(LONG_W) - min(LONG_W))) AS decimal(10,4)) 
+FROM station
+
+-- ORACLE
+select round(max(LAT_N)-min(LAT_N)+max(LONG_W)-min(LONG_W),4) from STATION;
+
+-- DB2
+SELECT
+    CAST(
+        ROUND(
+            ABS(MIN(LAT_N) - MAX(LAT_N)) +
+            ABS(MIN(LONG_W) - MAX(LONG_W)),
+            4
+        ) AS DECIMAL(20,4) 
+    )
+FROM STATION;
