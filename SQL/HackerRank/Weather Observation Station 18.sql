a631037b5c85784b6b245e3982728e48df200ed3
GEUNTAE SEO <74644453+gtseo0606@users.noreply.github.com>
2023-07-20 23:52:30 +0900

    Update Weather Observation Station 18.sql

--- SQL/HackerRank/Weather Observation Station 18.sql
+++ SQL/HackerRank/Weather Observation Station 18.sql
@@ -14,7 +14,7 @@ FROM POINTS
 
 
 -- MS SQL SERVER
-SELECT CAST(ABS((MAX(LAT_N) - min(LAT_N)) + (max(LONG_W) - min(LONG_W))) AS decimal(10,4)) 
+SELECT CAST(ABS((MAX(LAT_N) - min(LAT_N)) + (max(LONG_W) - MIN(LONG_W))) AS decimal(10,4)) 
 FROM station
 
 -- ORACLE
