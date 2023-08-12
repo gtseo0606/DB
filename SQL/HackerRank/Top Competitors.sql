279909856c09fbd2ecb474aaf8ce3c99d9a30f96
GEUNTAE SEO <74644453+gtseo0606@users.noreply.github.com>
2023-08-12 15:56:01 +0900

    Update Top Competitors.sql

--- SQL/HackerRank/Top Competitors.sql
+++ SQL/HackerRank/Top Competitors.sql
@@ -8,7 +8,7 @@ JOIN Challenges as c on s.challenge_id = c.challenge_id
 join Difficulty as d on d.difficulty_level = c.difficulty_level
 
 WHERE d.score = s.score
-group by hacker_id, h.name
+GROUP BY hacker_id, h.name
 having count(s.challenge_id) > 1
 
 order by count(s.challenge_id) desc, s.hacker_id;
