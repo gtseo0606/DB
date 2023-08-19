054627c6d8b1d4b62bbc4cd9aef60784b8ec4bd7
GEUNTAE SEO <74644453+gtseo0606@users.noreply.github.com>
2023-08-19 23:56:01 +0900

    Update Top Competitors.sql

--- SQL/HackerRank/Top Competitors.sql
+++ SQL/HackerRank/Top Competitors.sql
@@ -5,7 +5,7 @@ SELECT s.hacker_id, h.name
 from Hackers as h
 join Submissions as s  on s.hacker_id = h.hacker_id
 JOIN Challenges as c on s.challenge_id = c.challenge_id
-join Difficulty as d on d.difficulty_level = c.difficulty_level
+JOIN Difficulty as d on d.difficulty_level = c.difficulty_level
 
 WHERE d.score = s.score
 GROUP BY hacker_id, h.name
